943ace42c6ed71da07d4bc93db4a895cfd0274f0
unknown <castellano17@gmail.com>
2023-05-19 17:10:59 -0600

    add navbar

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/Header.css";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { token } = useSelector((store) => store.userInfo);
   return (
-    <header>
-      <h1>Planificador de Gastos</h1>
+    <header className="navbar">
+      <Link className="navbar__name" to="/">
+        <h1>Planificador de Gastos</h1>
+      </Link>
+
+      <div className="navbar__containerLinks">
+        <Link className="navbar__link" to="/">
+          <i className="home bx bx-home"></i>
+        </Link>
+        <Link className="user navbar__link" to="/login">
+          <i className="bx bx-user"></i>
+        </Link>
+      </div>
     </header>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./styles/Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <h1>Home</h1>
     </div>
   );
